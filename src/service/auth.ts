09778b5a52b60b47e29b2a63f5ce6d6f5b3fba06
Dharmvijay Patel <dharmvijay.patel@tatvasoft.com>
2022-09-09 12:15:53 +0530

    error handled

--- src/service/auth.ts
+++ src/service/auth.ts
@@ -3,22 +3,23 @@ import User from '../models/user.model';
 import environmentConfig from '../constants/environment.constant';
 import * as bcrypt from 'bcrypt';
 import jwt from 'jsonwebtoken';
+import { GenericErrorHandler } from '../utils/error.handler';
 export class AuthService {
  
     public static async login(req: Request, res: Response) {
         try {
           const { email, password } = req.body;
           if (!email || !password) {
-            return res.status(400).json({ message: 'please add all fields..!' });
+            throw new Error('validation failed');
           }
     
           const users = await User.findOne({ where: { email: email } });
           if (!users) {
-            return res.status(400).json({ message: 'You need to register first..!' });
+            throw new Error('Not Found');
           } else {
             const passwordMatched = bcrypt.compareSync(password, users.password);
             if (!passwordMatched) {
-              return res.status(401).json({ message: 'Invalid username or password' });
+              throw new Error('Unauthorized');
             } else {
               const token = jwt.sign({ id: users.id, role: users.role }, environmentConfig.JWT_SECRET);
               const data = {
@@ -33,17 +34,18 @@ export class AuthService {
               });
             }
           }
-        } catch (err) {
-          return res.status(500).json({ message: 'Something went wrong' });
+        } catch (error: any) {
+          const errorBody = await new GenericErrorHandler(error);
+          return res.status(200).json(errorBody);
         }
-      }
-      public static async register(req: Request, res: Response) {
+    }
+    public static async register(req: Request, res: Response) {
         try {
           const { email, password, role } = req.body;
     
           const users = await User.findOne({ where: { email: email } });
           if (users) {
-            return res.status(400).json({ message: 'User already exist..!' });
+            throw new Error('User already exist..!');
           } else {
             const hashPassword = await bcrypt.hashSync(password, 12);
             const newuser = await User.create({
@@ -52,14 +54,15 @@ export class AuthService {
               role,
             });
             if (!newuser) {
-              return res.status(401).json({ message: 'something went wrong..!' });
+              throw new Error('Internal Error');
             } else {
               return res.status(200).json({ message: 'New user registered successfully' });
             }
           }
-        } catch (err) {
-          return res.status(500).json({ message: 'Something went wrong' });
+        } catch (error: any) {
+          console.log(error)
+          const errorBody = await new GenericErrorHandler(error);
+          return res.status(200).json(errorBody);
         }
-      }
-
+    }
 }
--- src/service/user.ts
+++ src/service/user.ts
@@ -1,13 +1,13 @@
 import { Router, Request, Response, NextFunction } from 'express';
 import User from '../models/user.model';
-
+import { GenericErrorHandler } from '../utils/error.handler';
 export class UserService {
  
     public static async getUserList(req: Request, res: Response) {
         try {
             const users = await User.findAll({ where: { role: 'user' } , attributes: ['id', 'email','role','createdAt'] });
             if (!users) {
-                return res.status(400).json({ message: 'You need to register first..!' });
+                throw new Error('Not Found')
             } else {
                 return res.status(200).json({
                     message: 'Signin successfully',
@@ -15,8 +15,9 @@ export class UserService {
                     response_data: users,
                 });
             }
-        } catch (err) {
-          return res.status(500).json({ message: 'Something went wrong' });
+        } catch (error: any) {
+          const errorBody = await new GenericErrorHandler(error);
+          return res.status(200).json(errorBody);
         }
     }
 
